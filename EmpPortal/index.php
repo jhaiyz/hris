069e795b15af896407534be80aa6c5717e02e8ab
jhaiyz <74330397+jhaiyz@users.noreply.github.com>
2026-05-12 09:40:21 +0800

    Update index.php

--- EmpPortal/index.php
+++ EmpPortal/index.php
@@ -655,10 +655,8 @@ if (isLoggedIn()) {
     <div class="notice-banner">
         <svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
         <p>
-            If you do not yet have an employee number, you may proceed to the <strong>HR office</strong> for registration.
-            However, if you already have one, please click the
+            No employee number? Visit <strong>HR office</strong>. Already have one? Click Register.
             <button class="btn-register-inline" onclick="openRegModal()">Register</button>
-            button to continue your registration.
         </p>
     </div>
 
